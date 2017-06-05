fb77f92d3d35d1cd2534d3145994b981242fc32c
ericoni <ericoni94@gmail.com>
2017-06-06 00:21:58 +0200

    init

---
+++ BookingApp/BookingApp/Models/RoomReservations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingApp.Models
+{
+    public class RoomReservations
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Timestamp { get; set; }//kog tipa ovo da bude
+    }
+}
