e9651b3b7eefa66abc7fbf692966b316a6d38430
onetapgames <ivkovic.edu@gmail.com>
2017-06-12 13:52:04 +0200

    otulet

--- BookingApp/angull/src/app/app.module.ts
+++ BookingApp/angull/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+
+const Routes = [
+  {path: "users", component: UserComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(Routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
