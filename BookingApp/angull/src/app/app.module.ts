785f4ba3ff9f330def6b4f17fc8f517cb43bc77e
onetapgames <ivkovic.edu@gmail.com>
2017-06-12 23:06:25 +0200

    routing paths added

--- BookingApp/angull/src/app/app.module.ts
+++ BookingApp/angull/src/app/app.module.ts
@@ -4,15 +4,39 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { RoomReservationsComponent } from './roomReservation/roomReservation.component';
+import { RoomComponent } from './room/room.component';
+import { AccommodationTypeComponent } from './accommodationType/accommodationType.component';
+import { CommentComponent } from './comment/comment.component';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { RegionComponent } from './region/region.component';
+import { CountryComponent } from './country/country.component';
+import { PlaceComponent } from './place/place.component';
 
 const Routes = [
-  {path: "users", component: UserComponent}
+  {path: "users", component: UserComponent},
+   {path: "roomReservations", component: RoomReservationsComponent},
+   {path: "rooms", component: RoomComponent},
+   {path: "accommodationTypes", component: AccommodationTypeComponent},
+   {path: "comments", component: CommentComponent},
+   {path: "accommodations", component: AccommodationComponent},
+   {path: "regions", component: RegionComponent},
+   {path: "countries", component: CountryComponent},
+   {path: "places", component: PlaceComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    RoomReservationsComponent,
+    RoomComponent,
+    AccommodationTypeComponent,
+    CommentComponent,
+    AccommodationComponent,
+    RegionComponent,
+    CountryComponent,
+    PlaceComponent
   ],
   imports: [
     BrowserModule,
