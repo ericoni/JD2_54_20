94daad0f1d2670a49634ae4668bd3afb12ef345f
onetapgames <ivkovic.edu@gmail.com>
2017-06-21 02:52:43 +0200

    paginacija gotova

--- BookingApp/angull/src/app/accommodation/accommodation.component.ts
+++ BookingApp/angull/src/app/accommodation/accommodation.component.ts
@@ -24,7 +24,14 @@ export class AccommodationComponent implements OnInit {
   accommodationTypes: AccommodationType[];
   unapprovedAccommodations: Accommodation[];
   allAccommodations: Accommodation[];
+  allAccommodations1: Accommodation[];
+  allAccommodations2: Accommodation[];
+  allAccommodations3: Accommodation[];
   error: any;
+  i: number = 0;
+  j: number = 0;
+  pageNumber: number = 1;
+  accommcount: number;
   
   constructor(private httpAccommodationService: HttpAccommodationService, 
   private httpPlaceService: HttpPlaceService, private httpAccommodationTypeService: HttpAccommodationTypeService,
@@ -41,10 +48,30 @@ export class AccommodationComponent implements OnInit {
     //   (ap: any) => {this.accommodationTypes = ap; console.log(this.accommodationTypes)},
     //   error => {alert("Unsuccessful fetch operation!"); console.log(error);}
     // );
+    
     this.httpAccommodationTypeService.getAccommodationTypes().then(accommodationTypes => this.accommodationTypes = accommodationTypes).catch(error => this.error = error);
     this.httpPlaceService.getPlaces().then(places => this.places = places).catch(error => this.error = error);
     this.httpAccommodationService.getUnapprovedAccommodations().then(unapprovedAccommodations => this.unapprovedAccommodations = unapprovedAccommodations).catch(error => this.error = error);
-    this.httpAccommodationService.getAccommodations().then(allAccommodations => this.allAccommodations = allAccommodations).catch(error => this.error = error);
+    this.httpAccommodationService.getAccommodations()
+    .then(allAccommodations => {this.allAccommodations = allAccommodations; 
+      this.allAccommodations1 = new Array();
+      this.allAccommodations2 = new Array();
+      this.allAccommodations3 = new Array();
+      this.accommcount = allAccommodations.length;
+    for (var index = 0; index < Math.floor(this.accommcount/3); index++) {
+      this.allAccommodations1.push(this.allAccommodations[index]);
+      this.i = index;
+    }
+    for (var index1 = 0; index1 < Math.floor(this.accommcount/3); index1++) {
+      this.allAccommodations2.push(this.allAccommodations[this.i + index1 + 1]);
+      this.j = index1;
+    }
+    //this.j+=this.i;
+    for (var index2 = 0; index2 < this.accommcount-this.j-this.i - 2; index2++) {
+      this.allAccommodations3.push(this.allAccommodations[this.i + this.j + index2 + 2]);
+    }
+  }).catch(error => this.error = error);
+    debugger
     //this.authService.logout();
 }
 
@@ -53,6 +80,11 @@ export class AccommodationComponent implements OnInit {
     this.httpAccommodationService.postAccommodation(accommodation);
    //this.httpAccommodationService.postAccommodation(accomodation).subscribe(this.onPost);
     form.reset();
+  }
+
+  onPage(pageNum: any) {
+    this.pageNumber = pageNum;
+    debugger
   }
 
   onPost(res : any) : void{
